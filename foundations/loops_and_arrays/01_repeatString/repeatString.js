100fb17431c3dfe8298692888b7e12a6f5831832
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-20 22:19:42 +0000

    Correct repeatString.js to meet requirements

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeats) {
+    let new_string = "";
+    if (repeats < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < repeats ; i++) {
+        new_string = new_string + text;
+        }
+    }
+    return new_string;
 };
 
 // Do not edit below this line
