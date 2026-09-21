25ac7a82205bd6f953084fd96da3103ae4c4ad6e
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-21 21:24:59 +0200

    Edit function in removeFromArrays.js

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...otherArg) {
+    let new_array = [];
+
+    arr.forEach((element) => {
+        if (!otherArg.includes(element)) {
+            new_array.push(element);
+        }
+    });
+
+    return new_array;
 };
 
 // Do not edit below this line
