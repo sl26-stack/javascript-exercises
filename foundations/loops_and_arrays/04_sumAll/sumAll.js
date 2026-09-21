621f899711e92b7091c6bdc51e26f37b8f8e2115
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-21 21:49:23 +0200

    Create sumAll function in sumAll.js

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        if (b > a) {
+            let sum_val = 0;
+            for (let i = a; i <= b; i++) {
+                sum_val = sum_val + i;
+            }
+            return sum_val
+        } else {
+            let sum_val = 0;
+            for (let i = b; i <= a; i++) {
+            sum_val = sum_val + i;
+            }
+            return sum_val
+        }   
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
