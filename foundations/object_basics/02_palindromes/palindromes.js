b11c0ac8630dc94442f804744081c80e523cc760
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-22 16:35:28 +0200

    Created function to check palindromes

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const letOrNum = /[a-zA-Z0-9]/;
 
+    const cleanArrWord = word
+    .toLowerCase()
+    .split("")
+    .filter(item => letOrNum.test(item))
+    .join("");
+
+    let reversedWord = cleanArrWord.split("").reverse().join("");
+    return reversedWord === cleanArrWord;
 };
 
 // Do not edit below this line
