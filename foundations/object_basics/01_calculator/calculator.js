3233fc6104050041202ba327d06123a2153cfecd
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-22 15:46:10 +0200

    Completed functions in calculator.js & tested

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0,
+  );
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const initialValue = 1;
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator * currentValue, initialValue,
+  );
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (!num == 0) {
+    const arr = Array.from({length: num}, (value, index) => index + 1)
+    return arr.reduce(
+      (accumulator, currentValue) => accumulator * currentValue
+    )
+  } else {
+    return 1
+  }
 };
 
 // Do not edit below this line
