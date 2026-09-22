5c04e0038e1ccc97f6a204fee4c86831086f948d
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-22 18:33:49 +0200

    Create findTheOldest function

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleAges = people.map(person => ({
+        name: person.name,
+        age: (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth
+    }));
 
+    peopleAges.sort((a, b) => b.age - a.age);
+    
+    return peopleAges[0];
 };
 
 // Do not edit below this line
