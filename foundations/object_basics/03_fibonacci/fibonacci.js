d1ad8955281f0bfe07f22c477825661e1a4a498a
sl26-stack <327955617+sl26-stack@users.noreply.github.com>
2026-09-22 17:43:36 +0200

    Completed fibonacci seq function

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArr = [0];
+    num = +num;
+    if (num < 0 || !Number.isInteger(num)) {
+        return "OOPS"
+    } else {
+        for (let i = 0; fibArr.length <= num; i++) {
+            if (i == 0) {
+                fibArr.push(1);
+            } else {
+                fibArr.push(fibArr[i] + fibArr[i - 1]);
+            }
+        }
+    }
+    return fibArr[num];
 };
 
 // Do not edit below this line
